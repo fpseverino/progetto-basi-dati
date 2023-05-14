aaf1eca55c71efea4d20fd8e7fe8858edc689f2a
ImDennyy <96879323+ImDennyy@users.noreply.github.com>
2023-05-14 21:27:31 +0200

    Aggiunte Query
    Query da fare

--- query.sql
+++ query.sql
@@ -5,4 +5,12 @@
 --  Created by F.P. Severino, R. G. Scolari and D. Campisi on 14/05/23.
 -- 
 
+--1.	Impiegati che hanno acquistato una macchina
+--2.	Venditori/Meccanici/Contabili sottoposti di manager specifico
+--3.	Quali meccanici possono lavorare su macchina di cliente X
+--4.	Quante macchine ha venduto venditore X
+--5.	Chi ha venduto a cui ha lavorato meccanico X
+--6.	Auto vendute in sede X
+
+
 USE concessionaria;

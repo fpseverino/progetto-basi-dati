5ec4937ee53f84df469db27b3f4402d1bffa2392
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2023-05-14 18:33:27 +0200

    Initial definition

---
+++ definition.sql
@@ -0,0 +1,114 @@
+-- 
+--  definition.sql
+--  progettoBasiDati
+-- 
+--  Created by F.P. Severino, R. G. Scolari and D. Campisi on 14/05/23.
+-- 
+
+CREATE DATABASE IF NOT EXISTS concessionaria;
+
+CREATE TABLE IF NOT EXISTS Impiegato (
+    Matricola INT NOT NULL,
+    Nome VARCHAR(30) NOT NULL,
+    Cognome VARCHAR(30) NOT NULL,
+    DataNascita DATE NOT NULL,
+    CF VARCHAR(16) NOT NULL,
+    Contratto VARCHAR(30) NOT NULL,
+    Stipendio INT NOT NULL,
+    Sede INT NOT NULL,
+    Categoria VARCHAR(30) NOT NULL,
+    PRIMARY KEY (Matricola),
+    FOREIGN KEY (Sede) REFERENCES Sede(NumSede)
+);
+
+CREATE TABLE IF NOT EXISTS Sede (
+    NumSede INT NOT NULL,
+    Comune VARCHAR(30) NOT NULL,
+    Provincia VARCHAR(2) NOT NULL,
+    Indirizzo VARCHAR(30) NOT NULL,
+    PRIMARY KEY (NumSede)
+);
+
+CREATE TABLE IF NOT EXISTS Manager (
+    Impiegato INT NOT NULL,
+    Incarico VARCHAR(30) NOT NULL,
+    PRIMARY KEY (Impiegato),
+    FOREIGN KEY (Impiegato) REFERENCES Impiegato(Matricola)
+);
+
+CREATE TABLE IF NOT EXISTS Auto (
+    NTelaio INT NOT NULL,
+    Colore VARCHAR(30) NOT NULL,
+    KM INT NOT NULL,
+    Venduta BOOLEAN NOT NULL,
+    Modello VARCHAR(30) NOT NULL,
+    PRIMARY KEY (NTelaio),
+    FOREIGN KEY (Modello) REFERENCES Modello(Nome)
+);
+
+CREATE TABLE IF NOT EXISTS Disponibilita (
+    Auto INT NOT NULL,
+    Sede INT NOT NULL,
+    PRIMARY KEY (Auto, Sede),
+    FOREIGN KEY (Auto) REFERENCES Auto(NTelaio),
+    FOREIGN KEY (Sede) REFERENCES Sede(NumSede)
+);
+
+CREATE TABLE IF NOT EXISTS Modello (
+    Nome VARCHAR(30) NOT NULL,
+    Marchio VARCHAR(30) NOT NULL,
+    Porte INT NOT NULL,
+    Posti INT NOT NULL,
+    PrezzoCons INT NOT NULL,
+    Motore VARCHAR(30) NOT NULL,
+    PRIMARY KEY (Nome),
+    FOREIGN KEY (Motore) REFERENCES Motore(Nome)
+);
+
+CREATE TABLE IF NOT EXISTS Motore (
+    Nome VARCHAR(30) NOT NULL,
+    Alimentazione VARCHAR(30) NOT NULL,
+    Trasmissione VARCHAR(30) NOT NULL,
+    Trazione VARCHAR(30) NOT NULL,
+    CV INT NOT NULL,
+    PRIMARY KEY (Nome)
+);
+
+CREATE TABLE IF NOT EXISTS Specializzazione (
+    Impiegato INT NOT NULL,
+    Motore VARCHAR(30) NOT NULL,
+    PRIMARY KEY (Impiegato, Motore),
+    FOREIGN KEY (Impiegato) REFERENCES Impiegato(Matricola),
+    FOREIGN KEY (Motore) REFERENCES Motore(Nome)
+);
+
+CREATE TABLE IF NOT EXISTS Riparazione (
+    Impiegato INT NOT NULL,
+    Auto INT NOT NULL,
+    Data DATE NOT NULL,
+    Descrizione VARCHAR(30) NOT NULL,
+    PRIMARY KEY (Impiegato, Auto, Data, Descrizione),
+    FOREIGN KEY (Impiegato) REFERENCES Impiegato(Matricola),
+    FOREIGN KEY (Auto) REFERENCES Auto(NTelaio)
+);
+
+CREATE TABLE IF NOT EXISTS Cliente (
+    CodCliente INT NOT NULL,
+    Nome VARCHAR(30) NOT NULL,
+    Cognome VARCHAR(30) NOT NULL,
+    DataNascita DATE NOT NULL,
+    CF VARCHAR(16) NOT NULL,
+    PRIMARY KEY (CodCliente)
+);
+
+CREATE TABLE IF NOT EXISTS Vendita (
+    Impiegato INT NOT NULL,
+    Auto INT NOT NULL,
+    Cliente INT NOT NULL,
+    Data DATE NOT NULL,
+    Prezzo INT NOT NULL,
+    PRIMARY KEY (Impiegato, Auto, Cliente, Data),
+    FOREIGN KEY (Impiegato) REFERENCES Impiegato(Matricola),
+    FOREIGN KEY (Auto) REFERENCES Auto(NTelaio),
+    FOREIGN KEY (Cliente) REFERENCES Cliente(CodCliente)
+);
